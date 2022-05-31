92da1d1a7f8ac85d5f6c32fad4dd1aef58856fdd
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-31 14:30:09 +0200

    add title

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: "BaseTitle"
+}
+</script>
+
+<style scoped lang="scss">
+h1 {
+  color: red;
+}
+</style>
